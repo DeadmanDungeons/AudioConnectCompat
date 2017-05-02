fed3f1e1383a1cfd47679fa296493ddf62b9aea8
Jon <jon@jonmcpherson.com>
2017-05-02 17:25:39 -0400

    Initial Commit - AudioConnect compatibility with legacy WorldGuard versions

---
+++ src/com/deadmandungeons/audioconnect/flags/compat/FlagHandler.java
@@ -0,0 +1,13 @@
+package com.deadmandungeons.audioconnect.flags.compat;
+
+import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
+
+public interface FlagHandler<T> {
+	
+	T unmarshal(Object object);
+	
+	Object marshal(T instance);
+	
+	T parseInput(String input) throws InvalidFlagFormat;
+	
+}
---
+++ src/com/deadmandungeons/audioconnect/flags/compat/LegacyFlag.java
@@ -0,0 +1,42 @@
+package com.deadmandungeons.audioconnect.flags.compat;
+
+import org.bukkit.command.CommandSender;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
+import com.sk89q.worldguard.protection.flags.RegionGroup;
+import com.sk89q.worldguard.protection.flags.StringFlag;
+
+public class LegacyFlag<T> extends Flag<T> {
+	
+	private final StringFlag stringFlag = new StringFlag(null);
+	private final FlagHandler<T> handler;
+	
+	public LegacyFlag(FlagHandler<T> handler, String name, RegionGroup defaultGroup) {
+		super(name, defaultGroup);
+		this.handler = handler;
+	}
+	
+	public LegacyFlag(FlagHandler<T> handler, String name) {
+		super(name);
+		this.handler = handler;
+	}
+	
+	
+	@Override
+	public T parseInput(WorldGuardPlugin plugin, CommandSender sender, String input) throws InvalidFlagFormat {
+		return handler.parseInput(stringFlag.parseInput(plugin, sender, input));
+	}
+	
+	@Override
+	public T unmarshal(Object object) {
+		return handler.unmarshal(object);
+	}
+	
+	@Override
+	public Object marshal(T instance) {
+		return handler.marshal(instance);
+	}
+	
+}
