0a100bf10cdbdd0c8704ea458ffabcfee15041e0
Jon <jon@jonmcpherson.com>
2017-05-24 17:49:27 -0400

    Converts tabs to spaces
    I now see the light...
    From tabs to spaces and from Eclipse to IntelliJ IDEA

--- src/com/deadmandungeons/audioconnect/flags/compat/FlagHandler.java
+++ src/com/deadmandungeons/audioconnect/flags/compat/FlagHandler.java
@@ -3,11 +3,11 @@ package com.deadmandungeons.audioconnect.flags.compat;
 import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
 
 public interface FlagHandler<T> {
-	
-	T unmarshal(Object object);
-	
-	Object marshal(T instance);
-	
-	T parseInput(String input) throws InvalidFlagFormat;
-	
+
+    T unmarshal(Object object);
+
+    Object marshal(T instance);
+
+    T parseInput(String input) throws InvalidFlagFormat;
+
 }
